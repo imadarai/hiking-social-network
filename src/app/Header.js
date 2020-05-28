62734d04b6827fec1ab01d26477f06d827e5bbe2
imad <arain.imad@gmail.com>
2020-05-28 11:55:38 +0200

    cleaned up index.js

--- src/app/Header.js
+++ src/app/Header.js
@@ -10,7 +10,7 @@ export default function () {
                 <Link to="/usersearch"><button className="nav-button">Find Hikers</button></Link>
                 <Link to="/friends"><button className="nav-button">My Buddies</button></Link>
                 <Link to="/chat"><button className="nav-button">Global Chat</button></Link>
-                <Link href="/logout"><button className="nav-button">Logout</button></Link>
+                <Link to="/logout"><button className="nav-button">Logout</button></Link>
             </div>
         </div>
     );
